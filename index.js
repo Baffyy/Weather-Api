285bd6ded6bf1805c8f901cbc8c3575c8f8f8d24
Baffour Gyimah <baffourkgyimah.code@gmail.com>
2026-04-16 20:21:56 +0100

    Add final API data and styling

--- index.js
+++ index.js
@@ -23,14 +23,17 @@ app.post("/submit", async(req, res) => {
     try{
        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=2cab5511669b7edcf3b1621c629701d1`);
        const result = response.data[0];
-       const weather= await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${result.lat}&lon=${result.lon}&appid=2cab5511669b7edcf3b1621c629701d1`)
+       const weather= await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${result.lat}&lon=${result.lon}&appid=2cab5511669b7edcf3b1621c629701d1&units=metric`)
        const weatherResult= weather.data;
        console.log(weatherResult)
        res.render("weather.ejs", {city: result.name, 
         country: result.state,
         mainweather: weatherResult.weather[0].main,
         description: weatherResult.weather[0].description,
-        icon: weatherResult.weather[0].icon
+        pressure: weatherResult.main.pressure,
+        temp: weatherResult.main.temp,
+        humid: weatherResult.main.humidity,
+        windS: weatherResult.wind.speed
     }) 
     } catch (error) {
         console.log(error)
